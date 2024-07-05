1dfe85c7ace6bd0ed4d1c221f5ef8a5e65d44672
aoqing <aoqing@newhope.cn>
2024-07-05 14:18:20 +0800

    🔧 build: import ts错误提示
    使用 vue setup 语法糖，未 export default ，TS 语法检测会报错，新增文件声明

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+/**
+ * @description: vue文件类型声明
+ * ()=> import('@/views/index.vue') connot find module
+ */
+declare module '*.vue' {
+  import type { ComponentOptions } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: ComponentOptions | ComponentOptions['setup']
+  export default component
+}
